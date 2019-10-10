d3365cd2c0db823914410b321632f50dc53ab614
Anthony Warnick <junkwarnick@gmail.com>
2019-10-10 11:22:02 -0500

    getting json to parse and bind

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-import keys from "keys";
+'use strict';
+import {keys} from "./keys.js";
 
-console.log(keys);
+// console.log(keys.api_key);
+getWeather( keys.api_key , "64111");
+function getWeather( key, zip ){
+    let toFetch = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid="+key;
+    
+    
+    fetch(toFetch)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(myJson) {
+        console.log(myJson);
+    });
+  }
