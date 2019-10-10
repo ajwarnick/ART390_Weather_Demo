4d58ea7fddbca16a23d10c88ba6acd01b6853e00
Anthony Warnick <junkwarnick@gmail.com>
2019-10-10 11:23:57 -0500

    space

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ import {keys} from "./keys.js";
 
 // console.log(keys.api_key);
 getWeather( keys.api_key , "64111");
+
 function getWeather( key, zip ){
     let toFetch = "https://api.openweathermap.org/data/2.5/weather?zip="+zip+",us&appid="+key;
-    
     
     fetch(toFetch)
     .then(function(response) {
