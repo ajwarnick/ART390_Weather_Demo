bb9e5b28015f1445f7d998f6206c1753652c2316
Anthony Warnick <ajwarnick@gmail.com>
2019-10-10 13:30:33 -0500

    zip done

--- script.js
+++ script.js
@@ -1,41 +1,36 @@
 'use strict';
-import {keys} from "./keys.js";
+import { keys } from "./keys.js";
 
 
 let zip = "";
 const zip_input = document.getElementById('zip');
 zip_input.addEventListener('input', zipTest);
 
-if( getParameterByName('zip') ){
+if (getParameterByName('zip')) {
     zip = getParameterByName('zip');
 }
-
-
 
 function zipTest(e) {
     // log.textContent = e.target.value;
     let log = document.getElementById('zip_display');
 
-    var test_zip = e.target.value.replace(/\D/g,'');
+    var test_zip = e.target.value.replace(/\D/g, '');
     var regex = /(^\d{5}$)|(^\d{5}-\d{4}$)/g;
     var found = test_zip.match(regex);
 
-    if( found ){
+    if (found) {
         zip_input.blur();
-        if( found[0] !== zip){
+        if (found[0] !== zip) {
             zip = found[0];
             log.textContent = zip;
-            getWeather( keys.api_key , zip );
+            getWeather(keys.api_key, zip);
         }
-        
+
     }
 }
 
-
-
-
-function getWeather( k, z ){
-    let toFetch = "https://api.openweathermap.org/data/2.5/weather?zip="+z+",us&appid="+k;
+function getWeather(k, z) {
+    let toFetch = "https://api.openweathermap.org/data/2.5/weather?zip=" + z + ",us&appid=" + k;
     console.log(toFetch);
     // fetch(toFetch)
     // .then(function(response) {
@@ -44,18 +39,13 @@ function getWeather( k, z ){
     // .then(function(myJson) {
     //     console.log(myJson);
     // });
-  }
+}
 
 
 
+/* HELPER FUNCTIONS */
 
-  
-
-
-
-  /* HELPER FUNCTIONS */
-
-  function getParameterByName(name, url) {
+function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
